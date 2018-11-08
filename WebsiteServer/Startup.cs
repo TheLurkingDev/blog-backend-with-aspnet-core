b2b24f804f0054c1ff288aac445dc7168115fd3d
Darren Cook <thelurkingdev@gmail.com>
2018-11-08 18:59:06 -0500

    Added LoggerService to container. Added Serilog to logging pipeline via Startup.

--- WebsiteServer/Startup.cs
+++ WebsiteServer/Startup.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using LoggerService;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using System.IO;
 using WebsiteServer.Extensions;
 
@@ -22,12 +24,16 @@ namespace WebsiteServer
         {
             services.ConfigureCORS();
             services.ConfigureIISIntegration();
+            services.ConfigureLoggerService();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            LoggerManager.AddToLoggerFactory(loggerFactory);
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
