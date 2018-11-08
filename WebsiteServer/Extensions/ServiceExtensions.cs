14adffd4df987b35b828a12384a1f1bb663eae42
Darren Cook <thelurkingdev@gmail.com>
2018-11-07 23:30:12 -0500

    Added extension methods for configuring CORS and IIS integration.

--- WebsiteServer/Extensions/ServiceExtensions.cs
+++ WebsiteServer/Extensions/ServiceExtensions.cs
@@ -1,7 +1,29 @@
-﻿namespace WebsiteServer.Extensions
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebsiteServer.Extensions
 {
     public static class ServiceExtensions
     {
+        public static void ConfigureCORS(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder
+                    .AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials());
+            });
+        }
 
+        public static void ConfigureIISIntegration(this IServiceCollection services)
+        {
+            services.Configure<IISOptions>(options =>
+            {
+
+            });
+        }
     }
 }
