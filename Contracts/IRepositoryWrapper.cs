b098bb8077ca69aca1624e120503b68cda70743d
Darren Cook <thelurkingdev@gmail.com>
2018-11-25 19:02:24 -0500

    Added UserRepository to IRepositoryWrapper.

--- Contracts/IRepositoryWrapper.cs
+++ Contracts/IRepositoryWrapper.cs
@@ -5,5 +5,6 @@
         IBlogCategoryRepository BlogCategoryRepository { get; }
         IBlogPostRepository BlogPostRepository { get; }
         IWebsiteRepository WebsiteRepository { get;  }
+        IUserRepository UserRepository { get; }
     }
 }
