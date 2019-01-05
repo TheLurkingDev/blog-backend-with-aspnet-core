a85550c4e865735f67f02620e67c95f9fbc1fa61
Darren Cook <thelurkingdev@gmail.com>
2019-01-05 17:51:57 -0500

    Updated User to match schema changes.

--- Entities/DataModels/User.cs
+++ Entities/DataModels/User.cs
@@ -11,8 +11,8 @@ namespace Entities.DataModels
         [Column("UserID")]
         public Guid Id { get; set; }
 
-        [Required(ErrorMessage = "UserName is required")]
-        public string UserName { get; set; }
+        [Required(ErrorMessage = "Email is required")]
+        public string Email { get; set; }
 
         public UserRole Role { get; set; }
 
@@ -22,7 +22,7 @@ namespace Entities.DataModels
         public byte[] HashedPassword { get; set; }
         public byte[] Salt { get; set; }
 
-        public string Email { get; set; }
+        public string UserName { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Phone { get; set; }
